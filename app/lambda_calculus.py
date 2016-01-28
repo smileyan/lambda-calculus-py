0e7cbe7eb724441d924a13a1064afdf281d9fe52
smileyan <papayado@gmail.com>
2016-01-29 08:13:30 +1030

    Adding cond number

--- app/lambda_calculus.py
+++ app/lambda_calculus.py
@@ -3,7 +3,7 @@
 # lambda expressions: 
 # <expression> ::= <name> | <function> | <application>
 # lambda function:
-# <function> ::= lambda<name>.<body>
+# <function> ::= λ<name>.<body>
     # where <body> ::= <expression>
 # <application> ::= (<function expression> <argument expression>)
     # where
@@ -22,11 +22,88 @@ def apply():
 def identify2():
     return lambda x: apply()(identify())(x)
 
-def select_first():
+def _select_first_():
     return lambda first: lambda second: first
 
-def select_second():
+select_first = _select_first_()
+
+def _select_second_():
     return lambda first: lambda second: second
+
+select_second = _select_second_()
 
 def make_pair():
     return lambda first: lambda second: lambda func: func(first)(second)
+
+# Truth values and conditional expression
+#     <condition>?<expression>:<expression>
+
+def _cond_():
+    return lambda e1: lambda e2: lambda c: c(e1)(e2)
+cond = _cond_()
+
+def _true():
+    return select_first
+
+true = _true()
+
+def _false():
+    return select_second
+
+false = _false()
+
+# NOT <operand>
+
+def _not_():
+    # return lambda x: cond()(_false())(_true())(x)
+    return lambda x: x(false)(true)
+
+_not = _not_()
+
+# AND <operand> AND <operand>
+
+def _and_():
+    # return lambda x: lambda y: cond()(y)(_false())(x)
+    return lambda x: lambda y: x(y)(false)
+
+_and = _and_()
+
+# OR <operand> OR <operand>
+def _or_():
+    # return lambda x: lambda y: cond(true)(y)(x)
+    return lambda x: lambda y: x(true)(y)
+
+_or = _or_()
+
+def _zero_():
+    return identify()
+
+zero = _zero_()
+
+# succ = λn.λs((s false) n)
+def _succ_():
+    return lambda n: lambda s: s(false)(n)
+
+succ = _succ_()
+
+one = succ(zero)
+
+# one ==
+# (succ zero) == 
+# (λn.λs.((s false) n) zero) =>
+# λs.((s false) zero)
+
+two = succ(succ(zero))
+
+# two ==
+# (succ one) ==
+# (λn.λs.((s false) n) one) =>
+# λs.((s false) one) ==
+# λs.((s false) λs.((s false) zero))
+
+three = succ(two)
+
+def _iszero_():
+    return lambda n: n(select_first)
+
+iszero = _iszero_()
--- test/test_lambda_calculus.py
+++ test/test_lambda_calculus.py
@@ -2,7 +2,8 @@ import unittest
 
 from app.lambda_calculus import identify, self_apply, apply, identify2
 from app.lambda_calculus import select_first, select_second, make_pair
-
+from app.lambda_calculus import cond, true, false, _not, _and, _or
+from app.lambda_calculus import zero, succ, one, two, three, iszero
 
 class TestLambdaCulculus(unittest.TestCase):
 
@@ -20,9 +21,36 @@ class TestLambdaCulculus(unittest.TestCase):
         # self.assertEqual(1, apply()(identify())(self_apply()))
 
     def test_select_first(self):
-        self.assertEqual(1, select_first()(1)(2))
-        self.assertEqual(2, select_second()(1)(2))
+        self.assertEqual(1, select_first(1)(2))
+        self.assertEqual(2, select_second(1)(2))
 
     def test_make_pair(self):
-        self.assertEqual(1, make_pair()(1)(2)(select_first()))
-        self.assertEqual(2, make_pair()(1)(2)(select_second()))
+        self.assertEqual(1, make_pair()(1)(2)(select_first))
+        self.assertEqual(2, make_pair()(1)(2)(select_second))
+
+    def test_cond(self):
+        self.assertEqual(1, cond(1)(2)(true))
+        self.assertEqual(2, cond(1)(2)(false))
+    
+    def test_not(self):
+        self.assertEqual(1, cond(1)(2)(_not(false)))
+        self.assertEqual(2, cond(1)(2)(_not(true)))
+
+    def test_and(self):
+        self.assertEqual(true, _and(true)(true))
+        self.assertEqual(false, _and(false)(false))
+        self.assertEqual(false, _and(true)(false))
+        self.assertEqual(false, _and(false)(true))
+        self.assertEqual(1, cond(1)(2)(_and(true)(true)))
+        self.assertEqual(2, cond(1)(2)(_and(false)(true)))
+    
+    def test_or(self):
+        self.assertEqual(true, _or(false)(true))
+        self.assertEqual(true, _or(true)(true))
+        self.assertEqual(false, _or(false)(false))
+        self.assertEqual(true, _or(true)(true))
+
+    def test_numbers(self):
+        self.assertEqual(true, iszero(zero))
+        self.assertEqual(false, iszero(one))
+        self.assertEqual(false, iszero(two))
