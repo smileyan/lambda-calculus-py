301895fba675587c548949516327f7b4e5ebe42d
smileyan <papayado@gmail.com>
2016-05-18 07:57:56 +0930

    Adding imperative

--- app/imperative.fs
+++ app/imperative.fs
@@ -72,5 +72,75 @@ Chapter 8. Imperative Programming
           List.find_map t.buckets.(hash_bucket key)
             ~f:(fun (key',data) -> if key' = key then Some data else None)
 
+        Note that num_buckets is a constant, which means our bucket array is of fixed length. 
+        A practical implementation would need to be able to grow the array as the number of elements in the dictionary increases, but we'll omit this to simplify the presentation.
+
+        The function hash_bucket is used throughout the rest of the module to choose the position in the array that a given key should be stored at. 
+        It is implemented on top of Hashtbl.hash, which is a hash function provided by the OCaml runtime that can be applied to values of any type. 
+        Thus, its own type is polymorphic: 'a -> int.
+
+        The other functions defined above are fairly straightforward:
+
+        create
+        Creates an empty dictionary.
+
+        length
+        Grabs the length from the corresponding record field, thus returning the number of entries stored in the dictionary.
+
+        find
+        Looks for a matching key in the table and returns the corresponding value if found as an option.
+
+        Another important piece of imperative syntax shows up in find: we write array.(index) to grab a value from an array. 
+        find also uses List.find_map, which you can see the type of by typing it into the toplevel:
+
+        # List.find_map;;
+         - : 'a list -> f:('a -> 'b option) -> 'b option = <fun>
+
+        List.find_map iterates over the elements of the list, calling f on each one until a Some is returned by f, at which point that value is returned. 
+        If f returns None on all values, then None is returned.
+
+        Now let's look at the implementation of iter:
+
+        let iter t ~f =
+          for i = 0 to Array.length t.buckets - 1 do
+            List.iter t.buckets.(i) ~f:(fun (key, data) -> f ~key ~data)
+          done
+
+        iter is designed to walk over all the entries in the dictionary. 
+        In particular, iter t ~f will call f for each key/value pair in dictionary t. 
+        Note that f must return unit, since it is expected to work by side effect rather than by returning a value, and the overall iter function returns unit as well.
+
+        The code for iter uses two forms of iteration: a for loop to walk over the array of buckets; and within that loop a call to List.iter to walk over the values in a given bucket. 
+        We could have done the outer loop with a recursive function instead of a for loop, but for loops are syntactically convenient, and are more familiar and idiomatic in imperative contexts.
+
+        The following code is for adding and removing mappings from the dictionary:
+
+        let bucket_has_key t i key =
+          List.exists t.buckets.(i) ~f:(fun (key',_) -> key' = key)
+
+        let add t ~key ~data =
+          let i = hash_bucket key in
+          let replace = bucket_has_key t i key in
+          let filtered_bucket =
+            if replace then
+              List.filter t.buckets.(i) ~f:(fun (key',_) -> key' <> key)
+            else
+              t.buckets.(i)
+          in
+          t.buckets.(i) <- (key, data) :: filtered_bucket;
+          if not replace then t.length <- t.length + 1
+
+        let remove t key =
+          let i = hash_bucket key in
+          if bucket_has_key t i key then (
+            let filtered_bucket =
+              List.filter t.buckets.(i) ~f:(fun (key',_) -> key' <> key)
+            in
+            t.buckets.(i) <- filtered_bucket;
+            t.length <- t.length - 1
+          )
+
+
+
 
 
